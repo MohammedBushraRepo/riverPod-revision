30bd7c3014983edb4d1b43e9a4ea1cee8926601b
mohamed bushra <m.bushra9577@gmail.com>
2024-10-23 11:29:51 +0400

    Fetch from Api

--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -1,11 +1,24 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
 
-final dataProvider = FutureProvider((ref) => fetchData());
+final dataProvider = FutureProvider((ref) => fetchData()); //use FutureProvide.autoDisopse if you dont need to save the data in the Mobile cash  
 
 Future<String> fetchData() async {
-  await Future.delayed(Duration(seconds: 5));
-  return 'Mohamed Bushra AL Makkana';
+  // await Future.delayed(Duration(seconds: 5));
+  String url = 'https://jsonplaceholder.typicode.com/posts';
+  // Convert the String to a Uri
+  Uri uri = Uri.parse(url);
+
+  // Now use the Uri in the request
+  var response = await http.get(uri);
+  //decode the json data
+  var data = await jsonDecode(response.body);
+
+  //print(data[0]['body']);
+  return data[0]['body'];
 }
 
 class SecondScreen extends ConsumerWidget {
