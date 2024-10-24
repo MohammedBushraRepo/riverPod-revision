68a19bbbc482759649a9b8323c6625110aef3582
mohamed bushra <m.bushra9577@gmail.com>
2024-10-24 12:25:12 +0400

    CombiningTwoProviders

--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -6,65 +6,94 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 import 'package:riverpod_revision/user_model.dart';
 
-CollectionReference userData = FirebaseFirestore.instance
-    .collection('userData'); //from your firstore data base collection name
+final cityProvider = StateProvider<String?>((ref) => 'Canada');
 
-// final getUserData = FutureProvider<QuerySnapshot>((ref) => userData
-//     .get()); //use FutureProvide.autoDisopse if you dont need to save the data in the Mobile cash
-
-//creating StreamProvider
-// final getUserDataStram = StreamProvider<String>((ref) => Stream.periodic(
-//       Duration(milliseconds: 100),
-//       (value) => '$value',
-//     ));
-// to get Stram from firsbase
-final getUserDataStram =
-    StreamProvider<QuerySnapshot>((ref) => userData.snapshots());
+final weatherProvider = StateProvider<String>((ref) {
+  final city = ref.watch(cityProvider.notifier);
+  return city.state == 'Canada'
+      ? '24'
+      : city.state == 'Sudan'
+          ? '50'
+          : city.state == 'Qatar'
+              ? '30'
+              : 'null';
+});
 
 class SecondScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final city = ref.watch(cityProvider.notifier);
+    final weather = ref.watch(weatherProvider.notifier);
+
     return Scaffold(
       body: Container(
         child: Center(
-          child: _getUserDataStream(ref),
-        ),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            DropdownButton(
+                value: city.state,
+                items: [
+                  DropdownMenuItem(
+                    child: Text('Canada'),
+                    value: 'Canada',
+                  ),
+                  DropdownMenuItem(
+                    child: Text('Sudan'),
+                    value: 'Sudan',
+                  ),
+                  DropdownMenuItem(
+                    child: Text('Qatar'),
+                    value: 'Qatar',
+                  ),
+                ],
+                onChanged: (value) {
+                  if (value != null) {
+                    ref.read(cityProvider.notifier).state = value;
+                  }
+                }),
+            Text(
+              weather.state,
+              style: TextStyle(fontSize: 40, color: Colors.amber),
+            ),
+          ],
+        )),
       ),
     );
   }
 
-  //this function willl return widget of data column
-  Widget _getUserDataStream(WidgetRef ref) {
-    final stream = ref.watch(getUserDataStram);
-    return stream.when(
-      data: (QuerySnapshot value) {
-        // Check if the document list is not empty
-        if (value.docs.isNotEmpty) {
-          // Ensure the data is correctly accessed from the first document
-          Map<String, dynamic>? data =
-              value.docs[0].data() as Map<String, dynamic>?;
+  // //this function willl return widget of data column
+  // Widget _getUserDataStream(WidgetRef ref) {
+  //   final stream = ref.watch(getUserDataStram);
+  //   return stream.when(
+  //     data: (QuerySnapshot value) {
+  //       // Check if the document list is not empty
+  //       if (value.docs.isNotEmpty) {
+  //         // Ensure the data is correctly accessed from the first document
+  //         Map<String, dynamic>? data =
+  //             value.docs[0].data() as Map<String, dynamic>?;
 
-          if (data != null) {
-            UserModel userData = UserModel.fromMap(data);
-            // Continue with your logic
-            print(userData.toMap());
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(userData.name.toString()),
-                Text(userData.email.toString())
-              ],
-            );
-          }
-        }
-        return Text('No Data Found');
-      },
-      error: (value, stack) => Text('Error'),
-      loading: () {
-        return CircularProgressIndicator();
-      },
-    );
-  }
+  //         if (data != null) {
+  //           UserModel userData = UserModel.fromMap(data);
+  //           // Continue with your logic
+  //           print(userData.toMap());
+  //           return Column(
+  //             mainAxisAlignment: MainAxisAlignment.center,
+  //             children: [
+  //               Text(userData.name.toString()),
+  //               Text(userData.email.toString())
+  //             ],
+  //           );
+  //         }
+  //       }
+  //       return Text('No Data Found');
+  //     },
+  //     error: (value, stack) => Text('Error'),
+  //     loading: () {
+  //       return CircularProgressIndicator();
+  //     },
+  //   );
+  // }
 
 //  Widget _buildUserData(WidgetRef ref) {
 //     final future = ref.watch(getUserData);
